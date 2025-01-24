90f96fc263dfb80efc9c953eeae3a7553c9ac89f
Mohadese Akhoondy <m.akhoondy1381@gmail.com>
2025-01-25 01:19:50 +0330

    first connection to ai

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+from langchain_openai import ChatOpenAI
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+llm = ChatOpenAI(model="gpt-4o-mini",
+                 base_url="https://api.avalai.ir/v1",
+                 api_key="aa-f0QUZKi3ybTwtTRQhEUA8MI9OHt3aaYFX5gf3KliWw4ykU4t")
 
+result = llm.invoke("hello")
+print(result.content)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
